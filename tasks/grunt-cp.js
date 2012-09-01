2a48746d455ac2c7000c78dbd53e43e93f7b1e9d
Christian Verkerk <christianverkerk@gmail.com>
2012-08-31 21:30:38 -0700

    Made cp function synchronous

--- tasks/grunt-cp.js
+++ tasks/grunt-cp.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
     // TASKS
     // ==========================================================================
 
-    grunt.registerMultiTask('mv', 'Copies files recursively from one directory to another.', function () {
+    grunt.registerMultiTask('cp', 'Copies files recursively from one directory to another.', function () {
         console.log(this.data);
         console.log(this.target);
         if (!this.data) {return false; }
@@ -26,72 +26,90 @@ module.exports = function(grunt) {
         //     dest: '/dist/media'
         //   }
         //}
-        grunt.helper('mv', this.target, this.data);
+        grunt.helper('cp', this.target, this.data);
     });
 
     // ==========================================================================
     // HELPERS
     // ==========================================================================
 
-    grunt.registerHelper('mv', function (target, data) {
+    grunt.registerHelper('cp', function (target, data) {
         //
-        // Based off of: https://gist.github.com/562982
+        // Based off of: https://gist.github.com/562982 && wrench.js
         //
         var fs = require('fs');
-        function copyDirRecursive(srcDir, newDir, clbk) {
-            console.log(srcDir);
-            console.log(newDir);
-            console.log("About to call stat on " + srcDir);
-            fs.statSync(srcDir, function(err, stats) {
-               return console.log("Stat srcdir");
-                //if (err) return clbk(err);
-            });
-            console.log("Moving on");
-            console.log(stats);
-            /*
-                fs.mkdir(newDir, srcDirStat.mode, function(err){
-                    console.log("Made dir");
-                    if (err) return clbk(err);
-                    fs.readdir(srcDir, function(err, files){
-                        if (err) return clbk(err);
-                        (function copyFiles(err){
-                            if (err) return clbk(err);
-
-                            var filename = files.shift();
-                            if (filename === null || typeof filename == 'undefined')
-                                return clbk();
-
-                            var file = srcDir+'/'+filename,
-                                newFile = newDir+'/'+filename;
-
-                            fs.stat(file, function(err, fileStat){
-                                if (fileStat.isDirectory())
-                                    copyDirRecursive(file, newFile, copyFiles);
-                                else if (fileStat.isSymbolicLink())
-                                    fs.readlink(file, function(err, link){
-                                        fs.symlink(link, newFile, copyFiles);
-                                    });
-                                else
-                                    fs.readFile(file, function(err, data){
-                                        fs.writeFile(newFile, data, copyFiles);
-                                    });
-                            });
-                        })();
-                    });
-                });
-            });
-            */
-        };
         
+        function rmdirSyncRecursive(path, failSilent) {
+            var files;
+
+            try {
+                files = fs.readdirSync(path);
+            } catch (err) {
+                if(failSilent) return;
+                throw new Error(err.message);
+            }
+
+            /*  Loop through and delete everything in the sub-tree after checking it */
+            for(var i = 0; i < files.length; i++) {
+                var currFile = fs.lstatSync(path + "/" + files[i]);
+
+                if(currFile.isDirectory()) // Recursive function back to the beginning
+                    rmdirSyncRecursive(path + "/" + files[i]);
+
+                else if(currFile.isSymbolicLink()) // Unlink symlinks
+                    fs.unlinkSync(path + "/" + files[i]);
+
+                else // Assume it's a file - perhaps a try/catch belongs here?
+                    fs.unlinkSync(path + "/" + files[i]);
+            }
+
+            /*  Now that we know everything in the sub-tree has been deleted, we can delete the main
+                directory. Huzzah for the shopkeep. */
+            return fs.rmdirSync(path);
+        };
+
+        function copyDirSyncRecursive(sourceDir, newDirLocation, opts) {
+
+            if (!opts || !opts.preserve) {
+                try {
+                    if(fs.statSync(newDirLocation).isDirectory()) {
+                        rmdirSyncRecursive(newDirLocation);
+                    }
+                } catch(e) { }
+            }
+
+            /*  Create the directory where all our junk is moving to; read the mode of the source directory and mirror it */
+            var checkDir = fs.statSync(sourceDir);
+            try {
+                fs.mkdirSync(newDirLocation, checkDir.mode);
+            } catch (e) {
+                //if the directory already exists, that's okay
+                if (e.code !== 'EEXIST') throw e;
+            }
+
+            var files = fs.readdirSync(sourceDir);
+
+            for(var i = 0; i < files.length; i++) {
+                var currFile = fs.lstatSync(sourceDir + "/" + files[i]);
+
+                if(currFile.isDirectory()) {
+                    /*  recursion this thing right on back. */
+                    copyDirSyncRecursive(sourceDir + "/" + files[i], newDirLocation + "/" + files[i], opts);
+                } else if(currFile.isSymbolicLink()) {
+                    var symlinkFull = fs.readlinkSync(sourceDir + "/" + files[i]);
+                    fs.symlinkSync(symlinkFull, newDirLocation + "/" + files[i]);
+                } else {
+                    /*  At this point, we've hit a file actually worth copying... so copy it on over. */
+                    var contents = fs.readFileSync(sourceDir + "/" + files[i]);
+                    fs.writeFileSync(newDirLocation + "/" + files[i], contents);
+                }
+            }
+        };
+
         if (!data.src || !data.dest) {
             grunt.log.error().writeln("You must specify both a src and a dest for grunt-mv".yellow);
         } else {
-            copyDirRecursive(data.src, data.dest, function (err) {
-                console.log(err);
-            });
-            //, function (data) {
-            //    grunt.log.write('Successfully copied ' + data.src + ' to ' + data.dest);
-            //});
+            copyDirSyncRecursive(data.src, data.dest)
         }
     });
 };
